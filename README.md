6898f30295ced51e0bd7daf2b890bbf827e57385
Ndombou jerry <ndomboukamdem@gmail.com>
2025-07-19 04:49:51 +0100

    retrait de venv/ de versionnage

---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+## Configuration pour les developpeurs
+
+### Cloner le repository
+- git clone (url)
+cd projet/path/
+
+### Créer une branche de suivi pour develop
+- git checkout -b  develop origin/develop
--- cadastre/settings.py
+++ cadastre/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -72,12 +75,18 @@ WSGI_APPLICATION = 'cadastre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
+
 
 
 # Password validation
