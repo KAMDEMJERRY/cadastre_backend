cae8425c2dad25f4899379b2995cc94629ce4f71
Ndombou jerry <ndomboukamdem@gmail.com>
2025-07-23 04:26:47 +0100

    feature: Ajout des views.py  et du serializers.py de lotissement

--- lotissement/views.py
+++ lotissement/views.py
@@ -1,35 +1,64 @@
 from argparse import Action
 from django.shortcuts import get_object_or_404, render
 from rest_framework import viewsets
-from account.models import User
-from lotissement.models import Bloc, Lotissement, Parcelle
-from lotissement.serializers import BlocSerializer, LotissementSerializer, ParcelleSerializer
+from rest_framework.permissions import IsAuthenticated
+from account.models import IsAdministrateurCadastral, IsProprietaire, IsSuperAdministrateur, User
+from lotissement.models import Bloc, Lotissement, Parcelle, Rue
+from lotissement.serializers import BlocSerializer, LotissementSerializer, ParcelleSerializer, RueSerializer
 from rest_framework.decorators import action
+from rest_framework.permissions import DjangoModelPermissions
 
 # Create your views here.
 class LotissementViewSet(viewsets.ModelViewSet):
     queryset = Lotissement.objects.all()
     serializer_class = LotissementSerializer
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [IsAuthenticated()]
+        elif self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAuthenticated(), IsAdministrateurCadastral() | IsSuperAdministrateur()]
+        return [IsAuthenticated()]
 
+    
 class BlocViewSet(viewsets.ModelViewSet):
     queryset = Bloc.objects.all()
     serializer_class = BlocSerializer
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [IsAuthenticated()]
+        elif self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAuthenticated(), IsAdministrateurCadastral() | IsSuperAdministrateur()]
+        return [IsAuthenticated()]
+
 
 class ParcelleViewSet(viewsets.ModelViewSet):
     queryset = Parcelle.objects.all()
     serializer_class = ParcelleSerializer
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [IsAuthenticated()]
+        
+        elif self.action == 'create':
+            return [IsAuthenticated(), IsAdministrateurCadastral() | IsSuperAdministrateur()]
+        
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            return [IsAuthenticated(), IsAdministrateurCadastral() | IsSuperAdministrateur()]
+        
+        elif self.action == 'mes_parcelles':
+            return [IsAuthenticated(), IsProprietaire()]
+        
+        elif self.action == 'parcelles_utilisateur':
+            return [IsAuthenticated(), IsAdministrateurCadastral() | IsSuperAdministrateur()]
+        
+        return [IsAuthenticated()]
 
-    @action(detail=False, methods=['get'])
-    def mes_parcelles(self, request):
-        """Récupère toutes les parcelles de l'utilisateur connecté"""
-        parcelles = self.queryset.filter(proprietaire=request.user)
-        serializer = self.get_serializer(parcelles, many=True)
-        return Response(serializer.data)
-    
-    @action(detail=False, methods=['get'], url_path='user/(?P<user_id>[^/.]+)')
-    def parcelles_utilisateur(self, request, user_id=None):
-        """Récupère les parcelles d'un utilisateur spécifique (admin seulement)"""
-        user = get_object_or_404(User, id=user_id)
-        parcelles = self.queryset.filter(proprietaire=user)
-        serializer = self.get_serializer(parcelles, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser or user.groups.filter(name='administrateur').exists():
+            return Parcelle.objects.all()
+        return Parcelle.objects.filter(proprietaire=user)
+
+class RueViewSet(viewsets.ModelViewSet):
+    queryset = Rue.objects.all()
+    serializer_class = RueSerializer
+    permission_classes = [DjangoModelPermissions]
