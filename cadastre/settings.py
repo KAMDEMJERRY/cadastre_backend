5171102006296387b4dd06d792310c6dd9dc121a
Ndombou jerry <ndomboukamdem@gmail.com>
2025-09-02 14:56:24 +0100

    final

--- cadastre/settings.py
+++ cadastre/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
-    # 'django_filters',
+    'django_filters',
     'drf_yasg',
     'account',
     'lotissement',
